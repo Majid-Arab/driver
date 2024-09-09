5284eceb2a240c7ba0e2f1a3160907b90fe28cb4
Majid-Arab <arabmajid12@gmail.com>
2024-09-09 10:40:30 +0500

    onboarding active dots fixed

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -17,7 +17,7 @@ const Onboarding = () => {
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
         onPress={() => {
-          router.replace("/(auth)/sign-up");
+          router.replace("/(auth)/sing-up");
         }}
         className="w-full flex justify-end items-end p-5"
       >
@@ -31,7 +31,7 @@ const Onboarding = () => {
           <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
         }
         activeDot={
-          <View className="w-[32px] h-[4px] mx-1 bg-[#02286FF] rounded-full" />
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
         }
         onIndexChanged={(index) => setActiveIndex(index)}
       >
@@ -57,7 +57,7 @@ const Onboarding = () => {
         title={isLastSlide ? "Get Started" : "Next"}
         onPress={() =>
           isLastSlide
-            ? router.replace("/(auth0/sign-up")
+            ? router.replace("/(auth)/sing-up")
             : swiperRef.current?.scrollBy(1)
         }
         className="w-10/12 mt-10"
