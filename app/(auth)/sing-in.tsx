3e75bd6a6e0de5d09d118dc97e3c22a54c3a1f85
Majid-Arab <arabmajid12@gmail.com>
2024-09-09 15:22:32 +0500

    sing up singin page with styling

--- app/(auth)/sing-in.tsx
+++ app/(auth)/sing-in.tsx
@@ -1,12 +1,68 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, Text, View, ScrollView } from "react-native";
 
-const SingIn = () => {
+const SingIp = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onSignUpPress = async () => {};
+
   return (
-    <SafeAreaView>
-      <Text>Sing In</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute left-5 bottom-5 ">
+            Welcome 👋
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.person}
+            value={form.email}
+            onChange={(value) =>
+              setForm({ ...form, email: value.nativeEvent.text })
+            }
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.person}
+            value={form.password}
+            onChange={(value) =>
+              setForm({ ...form, password: value.nativeEvent.text })
+            }
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+
+          <Link
+            href="/(auth)/sing-up"
+            className="text-lg text-general-200 text-center mt-10"
+          >
+            <Text>Don't have an account? </Text>
+            <Text className="text-primary-500">Sing Up</Text>
+          </Link>
+
+          {/* Verification Modal */}
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
-export default SingIn;
+export default SingIp;
--- app/(auth)/sing-up.tsx
+++ app/(auth)/sing-up.tsx
@@ -1,11 +1,77 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, Text, View, ScrollView } from "react-native";
 
 const SingUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignUpPress = async () => {};
+
   return (
-    <SafeAreaView>
-      <Text>Sing Up</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute left-5 bottom-5 ">
+            Create Your Account
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChange={(value) =>
+              setForm({ ...form, name: value.nativeEvent.text })
+            }
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.person}
+            value={form.email}
+            onChange={(value) =>
+              setForm({ ...form, email: value.nativeEvent.text })
+            }
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.person}
+            value={form.password}
+            onChange={(value) =>
+              setForm({ ...form, password: value.nativeEvent.text })
+            }
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+
+          <Link
+            href="/(auth)/sing-in"
+            className="text-lg text-general-200 text-center mt-10"
+          >
+            <Text>Already have an account? </Text>
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+
+          {/* Verification Modal */}
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
