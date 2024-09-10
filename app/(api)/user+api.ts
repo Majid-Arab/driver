01a99cf5bc98006f0075bcfd02072d69a188c36b
Majid-Arab <arabmajid12@gmail.com>
2024-09-10 17:40:06 +0500

    api fixes

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -6,25 +6,34 @@ export async function POST(request: Request) {
     const { name, email, clerkId } = await request.json();
 
     if (!name || !email || !clerkId) {
-      return Response.json(
-        { error: "Missing required fields" },
-        { status: 400 }
+      return new Response(
+        JSON.stringify({ error: "Missing required fields" }),
+        { status: 400, headers: { "Content-Type": "application/json" } }
       );
     }
 
     const response = await sql`
-      INSERT INTO users (name, email, clerk_id)
-
-      VALUE {
-          ${name},
-          ${email},
-          ${clerkId}
-      }
+      INSERT INTO users (
+        name, 
+        email, 
+        clerk_id
+      )
+      VALUES (
+        ${name},
+        ${email},
+        ${clerkId}
+      )
     `;
 
-    return new Response(JSON.stringify({ data: response }), { status: 201 });
+    return new Response(JSON.stringify({ data: response }), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
-    console.log(error);
-    return Response.json({ error: error }, { status: 500 });
+    console.error(error);
+    return new Response(JSON.stringify({ error: error }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
