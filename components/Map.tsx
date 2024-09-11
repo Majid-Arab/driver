2d7bfe418e8b989e09fcbadd15ccf5d4ec76617c
Majid-Arab <arabmajid12@gmail.com>
2024-09-11 15:31:38 +0500

    map view setup added zustand

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,9 +1,24 @@
+import React from "react";
 import { Text, View } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
-const Map = () => (
-  <View>
-    <Text>Map</Text>
-  </View>
-);
+const Map = () => {
+  // const region = {};
+
+  return (
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      // initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
+      <Text>Map</Text>
+    </MapView>
+  );
+};
 
 export default Map;
