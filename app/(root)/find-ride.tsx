604b92e5c22318bf8905e3e67394c3a32318aad1
Majid-Arab <arabmajid12@gmail.com>
2024-11-14 17:12:11 +0500

    changed bottom slider size

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -15,7 +15,7 @@ const FindRide = () => {
     setUserLocation,
   } = useLocationStore();
   return (
-    <RideLayout title="Ride" snapPoints={["85%"]}>
+    <RideLayout title="Ride" snapPoints={["45%"]}>
       <View className="my-3">
         <Text className="text-lg font-JakartaSemiBold mb-3">From</Text>
         <GoogleTextInput
